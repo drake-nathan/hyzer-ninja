6d019c669c7595f2157941032e4df7b2723c1bca
Nathan Drake <nathandrakedrums@gmail.com>
2022-05-19 11:03:34 -0500

    remove tests from build

--- back-end/dist/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PROD = void 0;
-exports.PROD = process.env.NODE_ENV === 'production';
-//# sourceMappingURL=constants.js.map
